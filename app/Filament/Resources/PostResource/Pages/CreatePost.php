660530672fe931cb352729c0e8dd572b60c9a509
Patrick Boivin <pboivin@gmail.com>
2023-05-28 15:59:34 -0400

    Enable preview on post create

--- app/Filament/Resources/PostResource/Pages/CreatePost.php
+++ app/Filament/Resources/PostResource/Pages/CreatePost.php
@@ -5,8 +5,29 @@ namespace App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Pboivin\FilamentPeek\Pages\Actions\PreviewAction;
+use Pboivin\FilamentPeek\Pages\Concerns\HasPreviewModal;
 
 class CreatePost extends CreateRecord
 {
+    use HasPreviewModal;
+
     protected static string $resource = PostResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            PreviewAction::make(),
+        ];
+    }
+
+    protected function getPreviewModalView(): ?string
+    {
+        return 'post-show';
+    }
+
+    protected function getPreviewModalDataRecordKey(): ?string
+    {
+        return 'post';
+    }
 }
--- resources/views/components/post-meta.blade.php
+++ resources/views/components/post-meta.blade.php
@@ -1,4 +1,8 @@
 @props(['post'])
 
-Published on {{ $post->published_at->format('M jS, Y') }} —
-in <a href="{{ route('post.index', ['category' => $post->category->slug]) }}">{{ $post->category->name }}</a>
+@if ($post->published_at)
+    Published on {{ $post->published_at->format('M jS, Y') }} —
+    in <a href="{{ route('post.index', ['category' => $post->category->slug]) }}">{{ $post->category->name }}</a>
+@else
+    [Not published]
+@endif
