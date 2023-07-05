4b3d8026710015ecd8a8c362f795af03223bd307
Patrick Boivin <pboivin@gmail.com>
2023-07-05 12:05:02 -0400

    Cleanup

--- app/Models/ContactEntry.php
+++ app/Models/ContactEntry.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ContactEntry extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'email',
         'message',
