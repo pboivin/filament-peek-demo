e21b10a766a30ad3df8747fe383245dbcd2fe9d4
Patrick Boivin <pboivin@gmail.com>
2023-07-08 16:15:38 -0400

    Update image block

--- app/Filament/Blocks/Image.php
+++ app/Filament/Blocks/Image.php
@@ -4,6 +4,7 @@ namespace App\Filament\Blocks;
 
 use Filament\Forms\Components\Builder\Block;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 
 class Image
@@ -14,10 +15,26 @@ class Image
     ): Block {
         return Block::make($name)
             ->schema([
-                TextInput::make('url')->label('Image URL'),
-                FileUpload::make('image')->label('Image upload'),
-                TextInput::make('alt'),
-                TextInput::make('caption'),
-            ]);
+                TextInput::make('url')
+                    ->label('Image URL'),
+
+                FileUpload::make('image')
+                    ->label('Image upload'),
+
+                Select::make('ratio')
+                    ->options([
+                        '4-3' => '4/3',
+                        '3-4' => '3/4',
+                        'free' => 'free',
+                    ])
+                    ->afterStateHydrated(fn ($state, $set) => $state || $set('ratio', '4-3')),
+
+                TextInput::make('alt')
+                    ->columnSpanFull(),
+
+                TextInput::make('caption')
+                    ->columnSpanFull(),
+            ])
+            ->columns($context === 'form' ? 2 : 1);
     }
 }
--- resources/views/components/blocks/image.blade.php
+++ resources/views/components/blocks/image.blade.php
@@ -3,16 +3,23 @@
     'url' => false,
     'alt' => false,
     'caption' => false,
+    'ratio' => false,
 ])
 
 @php
     $src = $image ? Storage::url($image) : $url;
+
+    $ratioClass = match ($ratio ?: '4-3') {
+        '4-3' => 'aspect-[4/3]',
+        '3-4' => 'aspect-[3/4]',
+        default => '',
+    }
 @endphp
 
 @if ($src && $caption)
     <figure>
         <img
-            class="w-full"
+            class="w-full {{ $ratioClass }} object-cover object-center"
             src="{{ $src }}"
             @if ($alt) alt="{{ $alt }}" @endif
         >
@@ -20,7 +27,7 @@
     </figure>
 @elseif ($src)
     <img
-        class="w-full"
+        class="w-full {{ $ratioClass }}"
         src="{{ $src }}"
         @if ($alt) alt="{{ $alt }}" @endif
     >
