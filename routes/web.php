2997f923e89bbae84d03da21917536e800bb1c31
Patrick Boivin <pboivin@gmail.com>
2023-07-31 16:10:48 -0400

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
+use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'blog'], function () {
+    Route::get('/', [PostController::class, 'index'])->name('post.index');
+    Route::get('/{slug}', [PostController::class, 'show'])->name('post.show');
 });
+
+Route::get('/', [PageController::class, 'home'])->name('page.home');
+
+Route::get('/contact', [PageController::class, 'contact'])->name('page.contact');
+
+Route::get('/{slug}', [PageController::class, 'show'])->name('page.show');
