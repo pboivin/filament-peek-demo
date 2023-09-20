f2b4213d53fc220890c1dd64bda2786f3d316a7a
Patrick Boivin <pboivin@gmail.com>
2023-09-20 11:26:24 -0400

    Setup Dusk

---
+++ tests/Browser/LoginTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     */
+    public function testExample(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/')
+                    ->assertSee('Laravel');
+        });
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,15 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        $this->afterApplicationCreated(function () {
+            if (!file_exists(database_path('database_test.sqlite'))) {
+                touch(database_path('database_test.sqlite'));
+            }
+        });
+
+        parent::setUp();
+    }
 }
