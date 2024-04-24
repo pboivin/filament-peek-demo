e65328aedf15e706d04f7b8cb348b6bbfa7826ec
Patrick Boivin <pboivin@gmail.com>
2024-04-24 10:49:53 -0400

    Add 'login' route and redirect

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/login', '/admin/login')->name('login');
+
 Route::group(['prefix' => 'blog'], function () {
     Route::get('/', [PostController::class, 'index'])->name('post.index');
     Route::get('/{slug}', [PostController::class, 'show'])->name('post.show');
