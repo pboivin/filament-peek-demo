522137530b0d1f1f323b491d4f5d8175f8a66d6b
Patrick Boivin <pboivin@gmail.com>
2025-07-10 13:30:18 -0400

    Update to Laravel 12

--- public/js/app/admin-script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('peek:modal-opening', (e) => {
-    console.log('Preview modal opening...');
-});
-
-document.addEventListener('peek:modal-closing', (e) => {
-    console.log('Preview modal closing...');
-});
