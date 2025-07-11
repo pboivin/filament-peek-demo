c1affe5f0c6478d5eaf81d4070b94200d0a4776c
Patrick Boivin <pboivin@gmail.com>
2025-07-11 15:17:13 -0400

    Add builder previews

--- app/Filament/Resources/PageResource.php
+++ app/Filament/Resources/PageResource.php
@@ -12,13 +12,11 @@ use Filament\Actions\DeleteAction;
 use Filament\Actions\EditAction;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Resource;
-use Filament\Schemas\Components\Actions;
 use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Support\Str;
-use Pboivin\FilamentPeek\Forms\Actions\InlinePreviewAction;
 use Pboivin\FilamentPeek\Tables\Actions\ListPreviewAction;
 
 class PageResource extends Resource
@@ -53,14 +51,6 @@ class PageResource extends Resource
                             ->columnSpan(1)
                             ->afterStateUpdated(fn($set, $state) => $set('slug', Str::slug($state))),
                     ]),
-
-                Actions::make([
-                    // InlinePreviewAction::make()
-                    //     ->label('Open Content Editor')
-                    //     ->builderName('content')
-                ])
-                    ->columnSpanFull()
-                    ->alignEnd(),
 
                 PageContent::make('content')
                     ->required(),
--- app/Filament/Resources/PostResource/Pages/HasPostPreview.php
+++ app/Filament/Resources/PostResource/Pages/HasPostPreview.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Fields\PostContent;
-use App\Filament\Fields\PostFooter;
 use Filament\Schemas\Components\Component;
 use Pboivin\FilamentPeek\Pages\Actions\PreviewAction;
 use Pboivin\FilamentPeek\Pages\Concerns\HasBuilderPreview;
@@ -12,12 +11,12 @@ use Pboivin\FilamentPeek\Pages\Concerns\HasPreviewModal;
 trait HasPostPreview
 {
     use HasPreviewModal;
-    // use HasBuilderPreview;
+    use HasBuilderPreview;
 
     protected function getActions(): array
     {
         return [
-            PreviewAction::make()->label('Preview Changes'),
+            PreviewAction::make()->label('Preview Post'),
         ];
     }
 
@@ -33,28 +32,13 @@ trait HasPostPreview
 
     protected function getBuilderPreviewView(string $builderName): ?string
     {
-        return match ($builderName) {
-            'content_blocks' => 'post.preview-content',
-            'footer_blocks' => 'post.preview-footer',
-        };
+        return 'post.preview-content';
     }
 
     public static function getBuilderEditorSchema(string $builderName): Component|array
     {
-        return match ($builderName) {
-            'content_blocks' => PostContent::make(
-                name: 'content_blocks',
-                context: 'preview',
-            )
-                ->label('Content')
-                ->columnSpanFull(),
-
-            'footer_blocks' => PostFooter::make(
-                name: 'footer_blocks',
-                context: 'preview',
-            )
-                ->label('Footer')
-                ->columnSpanFull(),
-        };
+        return PostContent::make(name: 'content_blocks', context: 'preview')
+            ->label('Content')
+            ->columnSpanFull();
     }
 }
