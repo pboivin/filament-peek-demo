b9ccb2ced3e47f7e7e875afb97ab2ca36e513095
Patrick Boivin <pboivin@gmail.com>
2025-07-11 16:14:00 -0400

    Add awcodes/filament-sticky-header

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use Awcodes\StickyHeader\StickyHeaderPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -65,6 +66,9 @@ class AdminPanelProvider extends PanelProvider
             ->plugins([
                 FilamentPeekPlugin::make()
                     ->disablePluginStyles(),
+
+                StickyHeaderPlugin::make()
+                    ->stickOnListPages(false),
             ])
             ->viteTheme('resources/css/filament/admin/theme.css');
     }
