6188ed522977b5c5cf041d78fbe92de8125776eb
Patrick Boivin <pboivin@gmail.com>
2026-03-23 11:11:05 -0400

    wip

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/filament/admin/theme.css', 'resources/css/app.css', 'resources/js
+            input: ['resources/css/filament/admin/theme.css', 'resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
         tailwindcss(),
